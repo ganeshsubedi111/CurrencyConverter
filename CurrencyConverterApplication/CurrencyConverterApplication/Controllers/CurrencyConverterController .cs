2d91303ecbc01869bca822d024e3296d03f48c1a
ganeshsubedi111 <subediganesh306@gmail.com>
2024-11-26 20:23:05 +0545

    Currency converter

---
+++ CurrencyConverterApplication/CurrencyConverterApplication/Controllers/CurrencyConverterController .cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CurrencyConverterApplication.Models;
+using System.Threading.Tasks;
+using CurrencyConverterApplication.Models;
+
+public class CurrencyConverterController : Controller
+{
+    private readonly CurrencyConverterService _converterService;
+
+    public CurrencyConverterController(CurrencyConverterService converterService)
+    {
+        _converterService = converterService;
+    }
+
+    public IActionResult Index()
+    {
+        // Ensure you pass a new instance of CurrencyConverterModel to the view
+        return View(new CurrencyConverterModel());
+    }
+
+    [HttpPost]
+    public async Task<JsonResult> Convert(CurrencyConverterModel model, string apiKey)
+    {
+        // You can now use the apiKey to verify or log if needed
+        if (string.IsNullOrEmpty(apiKey) || apiKey != "cc697982a80a8386e462d")
+        {
+            return Json(new { error = "Invalid API key" });
+        }
+
+        if (ModelState.IsValid)
+        {
+            // Perform the conversion using your service
+            var result = await _converterService.ConvertCurrency(model.FromCurrency, model.ToCurrency, model.Amount);
+            return Json(new { convertedAmount = result }); // Return the converted amount as JSON
+        }
+
+        return Json(new { convertedAmount = 0 }); // Return 0 if there's an error or invalid data
+    }
+
+}
---
+++ CurrencyConverterApplication/CurrencyConverterApplication/Services/CurrencyConverterService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+public class CurrencyConverterService
+{
+    private static readonly string apiUrl = "https://api.exchangerate-api.com/v4/latest/";
+
+    public async Task<decimal> ConvertCurrency(string fromCurrency, string toCurrency, decimal amount)
+    {
+        var client = new HttpClient();
+        var response = await client.GetStringAsync(apiUrl + fromCurrency);
+
+        JObject exchangeRates = JObject.Parse(response);
+        decimal exchangeRate = exchangeRates["rates"][toCurrency].Value<decimal>();
+
+        return amount * exchangeRate;
+    }
+}
